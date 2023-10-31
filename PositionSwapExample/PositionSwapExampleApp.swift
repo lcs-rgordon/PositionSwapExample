cea38140f15938b7ba86beff60fb806ec2024b05
Russell Gordon <rgordon@lcs.on.ca>
2023-10-31 06:35:39 -0400

    Initial Commit

---
+++ PositionSwapExample/PositionSwapExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositionSwapExampleApp.swift
+//  PositionSwapExample
+//
+//  Created by Russell Gordon on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct PositionSwapExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
